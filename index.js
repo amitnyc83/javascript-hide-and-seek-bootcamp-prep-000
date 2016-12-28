d4db5dd580fc3e396892e5a1135079c33d54a0ff
Amit Singh <amit_nyc@live.com>
2016-12-28 17:25:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+      }
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+  }
+function increaseRankBy(n){
+  const lis =document.querySelectorAll('.ranked-list')
+  for (let i = 0, l = lis.length; i < l; i++)
+  {
+    lis[i].innerHTML= (i + n).toString()
+  }
+  return lis
+}
+function deepestChild(){
+  return document.querySelector('div#grand-node')
+}
